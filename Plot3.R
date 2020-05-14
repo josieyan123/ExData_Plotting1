01f997974a47a5577354278ab38aed971f56f84b
josieyan123 <63740398+josieyan123@users.noreply.github.com>
2020-05-14 09:46:43 -0400

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,16 +1,19 @@
 household_power_consumption <- read.csv("~/Downloads/household_power_consumption.txt", sep=";") ##read dataset into R session
-usefuldata <- household_power_consumption[66637:69516, ] ##subset the data for 2017-02-01 & 2017-02-02
+
+##subset the data for 2007-02-01 & 2007-02-02
+usefuldata <- subset(household_power_consumption, household_power_consumption$Date == "1/2/2007" | household_power_consumption$Date == "2/2/2007")
 
 ##convert Date & Time variables to Date/Time class in R using lubridate package
-library(lubridate)  
-usefuldata$Date = dmy(usefuldata$Date)  
-usefuldata$Time = strptime(usefuldata$Time, format = "%H:%M:%S")
+usefuldata$Date = as.Date(usefuldata$Date, format="%d/%m/%Y")
+usefuldata$Time = strptime(usefuldata$Time, format="%H:%M:%S")
+usefuldata[1:1440,"Time"] = format(usefuldata[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+usefuldata[1441:2880,"Time"] = format(usefuldata[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
 png(file = "plot3.png")  #give the plot a name 
-y <- usefuldata[ ,7:9]
-plot(y[,1],ylim=c(0,30),col="black",lty=1,ylab="Energy sub metering",lwd=2,xlab="Date",xaxt="n")
-lines(y[,2],col="red",lty=1,lwd=2)
-lines(y[,3],col="blue",lty=1,lwd=2)
+plot(usefuldata$Time,usefuldata$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(usefuldata, lines(usefuldata$Time,as.numeric(as.character(Sub_metering_1))))
+with(usefuldata,lines(usefuldata$Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(usefuldata, lines(usefuldata$Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
 legend("topright", pch = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 dev.off() #save the file
