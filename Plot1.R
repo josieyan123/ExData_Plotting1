8d4992852d34e02ec2d57b201ef7a3f56ba79d11
josieyan123 <63740398+josieyan123@users.noreply.github.com>
2020-05-14 09:45:50 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,17 +1,18 @@
 household_power_consumption <- read.csv("~/Downloads/household_power_consumption.txt", sep=";") ##read dataset into R session
-usefuldata <- household_power_consumption[66637:69516, ] ##subset the data for 2017-02-01 & 2017-02-02
+
+##subset the data for 2007-02-01 & 2007-02-02
+usefuldata <- subset(household_power_consumption, household_power_consumption$Date == "1/2/2007" | household_power_consumption$Date == "2/2/2007") 
+
 
 ##convert Date & Time variables to Date/Time class in R using lubridate package
 library(lubridate)  
 usefuldata$Date = dmy(usefuldata$Date)  
 usefuldata$Time = strptime(usefuldata$Time, format = "%H:%M:%S")
 
-library(datasets)
-counts <- table(usefuldata$Global_active_power)  ##subsetting data for the graph 
 png(file = "plot1.png")  ##give the plot a name 
 
 ##plotting the graph
-hist(counts, xlab = "Global Active Power(Kilowatts)", ylab = "Frequency", col = "red", xlim = c(0,6), luylim = c(0,1200), main = "Global Active Power")
+hist(as.numeric(as.character(usefuldata$Global_active_power)), xlab = "Global Active Power(Kilowatts)", ylab = "Frequency", col = "red", main = "Global Active Power")
 
 ##save the file 
 dev.off()
